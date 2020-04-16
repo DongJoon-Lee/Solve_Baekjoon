2febe737f29d264db876c51c2147f8e0ec66aca9
DongJoon-Lee <43167798+DongJoon-Lee@users.noreply.github.com>
2020-04-16 19:00:42 +0900

    4/14 - 4월 14일에 푼 분할정복 문제입니다. 하루에 하나 정도만..

---
+++ 1. 날짜/200414/1629.cpp
@@ -0,0 +1,49 @@
+# include <string>
+# include <iostream>
+# include <vector>
+# include <queue>
+# include <stdio.h>
+# include <algorithm>
+# include <ctype.h>
+# include <cstdlib>
+#include <queue>
+# include <cmath>
+using namespace std;
+typedef long long ll;
+# define pb push_back
+# define mp make_pair
+
+const int MAX = 2147483647;
+const int MIN = -2147483648;
+
+ll A, B, C;
+
+
+int cal(ll A, ll B) {
+    if (B == 1) {
+        return A%C;
+    }
+    else if (B == 0) {
+        return 1;
+    }
+    else {
+        ll D = cal(A, B/2);
+        ll E = D*D%C;
+        if (B%2 == 0) {
+            return E;
+        }
+        else {
+            return E*A%C;
+        }
+    }
+}
+
+int main (void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> A >> B >> C;
+    cout << cal(A, B)<<"\n";
+
+    return 0;
+}
